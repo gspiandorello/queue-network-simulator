d3cbbb196ef54de5ba29b67fd6ef194b01f7d975
Gabriel Spiandorello <gabrielspiandorello@gmail.com>
2024-04-29 02:51:28 -0300

    Using queueClass

--- queueSimulator.py
+++ queueSimulator.py
@@ -3,60 +3,55 @@ from random_generator import PseudoRandomNumberGenerator
 
 
 class queueSimulator:
-    def __init__(self, quantityRandomNumbers, seed, minArrival, maxArrival, minService, maxService, queueServers, queueCapacity, initialTime):
+    def __init__(self, quantityRandomNumbers, seed, firstArrivalTime, queue):
         self.prng = PseudoRandomNumberGenerator(quantityRandomNumbers, seed)
         self.quantityRandomNumbers = quantityRandomNumbers
-        self.initialEventTime = initialTime
-        self.queue = []
+        self.firstArrivalTime = firstArrivalTime
+        self.queue = queue
+        self.schedule = []
         self.eventHeap = []
-        self.accumulatedTimes = [0] * (queueCapacity + 1)
-        self.queueCapacity = queueCapacity
-        self.queueServers = queueServers
-        self.minArrival = minArrival
-        self.maxArrival = maxArrival
-        self.minService = minService
-        self.maxService = maxService
-        self.losses = 0
         self.currentClient = 0
-        self.lastTimeEvent = 0
+        self.times = 0
 
     def timeGenerator(self, min, max):
         randomNumber = self.prng.generate()
         return min + ((max - min) * randomNumber)
 
     def accumulateTime(self, eventTime):
-        queueState = len(self.queue)
-        self.accumulatedTimes[queueState] += (eventTime - self.lastTimeEvent)
-        self.lastTimeEvent = eventTime
+        scheduleState = len(self.schedule)
+        self.queue.timeAtService[scheduleState] += (
+            eventTime - self.times)
+        self.times = eventTime
 
     def processArrival(self, eventTime):
-        if len(self.queue) < self.queueCapacity:
-            self.queue.append(self.currentClient)
-            if len(self.queue) <= self.queueServers:
+        if len(self.schedule) < self.queue.capacity:
+            self.schedule.append(self.currentClient)
+            if len(self.schedule) <= self.queue.servers:
                 departureTime = eventTime + \
-                    self.timeGenerator(self.minService,
-                                       self.maxService)
+                    self.timeGenerator(self.queue.minService,
+                                       self.queue.maxService)
                 heapq.heappush(self.eventHeap, (departureTime,
                                'departure', self.currentClient))
         else:
-            self.losses += 1
+            self.queue.losses += 1
         self.currentClient += 1
         nextArrivalTime = eventTime + \
-            self.timeGenerator(self.minArrival, self.maxArrival)
+            self.timeGenerator(self.queue.minArrival, self.queue.maxArrival)
         heapq.heappush(self.eventHeap, (nextArrivalTime,
                        'arrival', self.currentClient))
 
-    def processDeparture(self, eventTime, clientId):
-        self.queue.pop(0)
-        if self.queue and len(self.queue) >= self.queueServers:
-            nextClient = self.queue[self.queueServers - 1]
+    def processDeparture(self, eventTime):
+        self.schedule.pop(0)
+        if self.schedule and len(self.schedule) >= self.queue.servers:
+            nextClient = self.schedule[self.queue.servers - 1]
             nextDepartureTime = eventTime + \
-                self.timeGenerator(self.minService, self.maxService)
+                self.timeGenerator(self.queue.minService,
+                                   self.queue.maxService)
             heapq.heappush(self.eventHeap, (nextDepartureTime,
                            'departure', nextClient))
 
     def simulate(self):
-        heapq.heappush(self.eventHeap, (self.initialEventTime,
+        heapq.heappush(self.eventHeap, (self.firstArrivalTime,
                        'arrival', self.currentClient))
 
         while self.quantityRandomNumbers > 0 and self.eventHeap:
@@ -68,15 +63,15 @@ class queueSimulator:
             if eventType == 'arrival':
                 self.processArrival(eventTime)
             elif eventType == 'departure':
-                self.processDeparture(eventTime, currentClient)
+                self.processDeparture(eventTime)
 
             self.quantityRandomNumbers -= 1
 
         print("Simulation Results:")
-        for i in range(self.queueCapacity + 1):
-            probability = self.accumulatedTimes[i] / self.lastTimeEvent * 100
+        for i in range(self.queue.capacity + 1):
+            probability = self.queue.timeAtService[i] / self.times * 100
             print(f"State {i}: Accumulated time {
-                self.accumulatedTimes[i]:.4f}, Probability {probability:.2f}%")
+                self.queue.timeAtService[i]:.4f}, Probability {probability:.2f}%")
 
-        print(f"Lost customers: {self.losses}")
-        print(f"Total simulation time: {self.lastTimeEvent:.4f}\n")
+        print(f"Lost customers: {self.queue.losses}")
+        print(f"Total simulation time: {self.times:.4f}\n")
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from queueSimulator import queueSimulator
 from queueClass import Queue
 
-# q1 = Queue('q1', 1, 5, 2, 5, 3, 5)
-# q2 = Queue('q2', 2, 5, 2, 5, 3, 5)
+q1 = Queue('q1', 1, 5, 2, 5, 3, 5)
+q2 = Queue('q2', 2, 5, 2, 5, 3, 5)
 
-simulator1 = queueSimulator(100000, 12345, 2, 5, 3, 5, 1, 5, 2)
-simulator2 = queueSimulator(100000, 12345, 2, 5, 3, 5, 2, 5, 2)
+simulator1 = queueSimulator(100000, 12345, 2, q1)
+simulator2 = queueSimulator(100000, 12345, 2, q2)
 
 print("Simulation 1: G/G/1/5")
 simulator1.simulate()
